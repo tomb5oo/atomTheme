2d49792f49e79fdbfca92c569acff29131c3cbee
Ashley Roberts <ashley@Ashleys-MacBook-Air.local>
2019-02-18 14:41:21 +0000

    changed logo text in nav

--- view/pages/nav.php
+++ view/pages/nav.php
@@ -7,7 +7,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">tom is gay<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">Logo<span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Link</a>
