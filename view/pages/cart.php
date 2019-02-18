44a5d70c044530b72c70ed799ba6c76a6f95a743
Tom Bennett <urbanviolet@Toms-MacBook-Pro.local>
2019-02-18 22:51:26 +0000

    start cart php controls

--- view/pages/cart.php
+++ view/pages/cart.php
@@ -16,28 +16,12 @@
 			</tr>
 		</thead>
 		<tbody>
-			<tr>
-				<td data-th="Product">
-					<div class="row">
-						<div class="col-sm-2 hidden-xs"><img src="http://placehold.it/100x100" alt="..." class="img-responsive"/></div>
-						<div class="col-sm-10">
-							<h4 class="nomargin">Product 1</h4>
-							<p>Atom theme product description goes here...Atom theme product description goes here...Atom theme product description goes here </p>
-						</div>
-					</div>
-				</td>
-				<td data-th="Price">$1.99</td>
-				<td data-th="Quantity">
-					<input type="number" class="form-control text-center" value="1">
-				</td>
-				<td data-th="Subtotal" class="text-center">1.99</td>
-				<td class="actions" data-th="">
-					<button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
-					<button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>
-				</td>
-			</tr>
 
-			<tr>
+      <?php productImage(); ?>
+
+<!-- MOVED TO CONTOLS -->
+
+			<!-- <tr>
 				<td data-th="Product">
 					<div class="row">
 						<div class="col-sm-2 hidden-xs"><img src="http://placehold.it/100x100" alt="..." class="img-responsive"/></div>
@@ -56,7 +40,8 @@
 					<button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
 					<button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>
 				</td>
-			</tr>
+			</tr> -->
+
 		</tbody>
 		<tfoot>
 			<tr class="visible-xs">
