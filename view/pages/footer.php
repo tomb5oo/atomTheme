1012f7d1ee3bba1fc6c3e895a019ebd686e449e8
Ashley <ash.roberts121@gmail.com>
2019-04-06 11:42:40 +0100

    added media and node_modules folders

---
+++ view/pages/footer.php
@@ -0,0 +1,27 @@
+    <div class="row">
+      <footer class="page-footer font-small pt-4" style="background-color: lightgrey;">
+        <div class="container-fluid text-center text-md-left">
+          <div class="row">
+            <div class="col-md-6 mt-md-0 mt-3">
+              <h5 class="text-uppercase font-weight-bold">Footer text 1</h5>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita sapiente sint, nulla, nihil repudiandae commodi voluptatibus corrupti animi sequi aliquid magnam debitis, maxime quam recusandae harum esse fugiat. Itaque, culpa?</p>
+            </div>
+            <hr class="clearfix w-100 d-md-none pb-3">
+            <div class="col-md-6 mb-md-0 mb-3">
+              <h5 class="text-uppercase font-weight-bold">Footer text 2</h5>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio deserunt fuga perferendis modi earum commodi aperiam temporibus quod nulla nesciunt aliquid debitis ullam omnis quos ipsam, aspernatur id excepturi hic.</p>
+            </div>
+          </div>
+          <div class="footer-copyright text-center py-3">© 2018 Copyright:
+            <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+          </div>
+        </div>
+      </footer>
+    </div>
+
+  </body>
+
+  <!--Custom JavaScript-->
+  <script src="<?php echo DIR?>media/scripts/main.js"></script>
+
+</html>
---
+++ view/pages/header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+
+    <?php include('../../admin/config.php'); // Include config file
+          include("../../controller/controls.php");?>
+
+    <!--Character Set-->
+    <meta charset="utf-8">
+
+    <!--Site Title-->
+    <title><?php echo SITE_TITLE ?></title>
+
+    <!--Meta Tags-->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!--Font Awesome CDN-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+    <!-- viewport -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--Custom css-->
+    <link rel="stylesheet" href="<?php echo DIR?>media/styles/css/main.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo DIR?>media/styles/css/login.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo DIR?>media/styles/css/itempage.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo DIR?>media/styles/css/cart.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo DIR?>media/styles/css/home.css" type="text/css">
+
+    <!--Custom SCSS -->
+    <link rel="stylesheet" href="<?php echo DIR?>media/styles/sass/main.scss" type="text/css">
+    <link rel="stylesheet" href="<?php echo DIR?>media/styles/sass/login.scss" type="text/css">
+    <link rel="stylesheet" href="<?php echo DIR?>media/styles/sass/itempage.scss" type="text/css">
+    <link rel="stylesheet" href="<?php echo DIR?>media/styles/sass/cart.scss" type="text/css">
+    <link rel="stylesheet" href="<?php echo DIR?>media/styles/sass/home.scss" type="text/css">
+    <!--Font Awesome Link-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
+    <!--Bootstrap Links-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script><!--Bootstrap JQuery CDN-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"><!--Bootstrap CSS CDN-->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script><!--Bootstrap JS CDN-->
+
+    <!--jQuery CDN-->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+  </head>
+  <body>
