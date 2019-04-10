3afab4b68e5ece849cfaefb02f8cd0431bbe8a51
Tom Bennett <urbanviolet@Toms-MBP.lan>
2019-04-10 16:01:19 +0100

    deleted advanced custom fields

---
+++ atomTheme/wordpress/wp-content/themes/atomtheme/config.php
@@ -0,0 +1,22 @@
+<?php
+//Assign database details to variables
+define('DBHOST', 'localhost');
+define('DBUSER', 'root');
+define('DBPASS', 'techn000');
+define('DBNAME', 'atomtheme');
+
+//Setup database connection
+$conn = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
+
+//Check if connection was successful
+if(!$conn){
+    die("<span style='color:red;'>Error connecting to database</span>");
+}else{
+    echo "connected successfully";
+}
+
+session_start(); //Run session
+
+/*********************************************/
+
+/*********************************************/
