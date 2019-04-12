65818ed44daca43995c896034e487329bcff53a8
Tom Bennett <urbanviolet@Toms-MacBook-Pro.local>
2019-04-12 11:59:00 +0100

    ...

--- wordpress/wp-content/themes/atomtheme/page-contact.php
+++ wordpress/wp-content/themes/atomtheme/page-contact.php
@@ -52,6 +52,7 @@ get_header(); ?>
               <button type="submit" class="button button-contactForm">Send Message</button>
             </div>
           </form>
+          
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d152201.77188059303!2d-3.0561408714013165!3d53.41209544328504!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487adf8a647060b7%3A0x42dc046f3f176e01!2sLiverpool!5e0!3m2!1sen!2suk!4v1555062039212!5m2!1sen!2suk"
           width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 
